d666d065de08c4af3faf2b5262cfcd984241bd14
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-09-13 11:54:49 +0800

    test

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <analogWrite.h>
+
+uint8_t oe = 27;
+uint8_t latch = 25;
+uint8_t reset = 24;
+
+void setup() {
+    SPI.begin();
+    // digitalWrite(latch, LOW);
+    // SPI.transfer(0b1);
+    // digitalWrite(latch, HIGH);
+    Serial.begin(9600);
+    pinMode(oe, OUTPUT);
+    pinMode(latch, OUTPUT);
+    pinMode(reset, OUTPUT);
+    pinMode(2, OUTPUT);
+    digitalWrite(oe, LOW);
+    digitalWrite(latch, LOW);
+    digitalWrite(reset, HIGH);
+}
+void Update();
+SPISettings setting(10000000, MSBFIRST, SPI_MODE0);
+
+uint8_t data = 0;
+bool increasing = false;
+void loop() {
+    Update();
+    if (Serial.available())
+    {
+        char in = Serial.read();
+        data = in - '0';
+        Serial.print(data);
+    }
+    uint16_t touch = touchRead(15);
+    analogWrite(LED_BUILTIN, map(65 - touch, 0, 65, 0, 255));
+}
+
+void Update()
+{
+    static uint32_t lastUpdateTime = 0;
+    if (millis() - lastUpdateTime < 100) return;
+    lastUpdateTime = millis();
+
+    SPI.beginTransaction(setting);
+    digitalWrite(latch, LOW);
+    SPI.transfer(1 << data);
+    digitalWrite(latch, HIGH);
+    SPI.endTransaction();
+}
