f0f971da64587d2989708effb3ef64b8755e867c
Alan72104 <58733305+Alan72104@users.noreply.github.com>
2021-12-20 06:03:22 +0800

    Basic 3d data shifting and multiplexing

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,103 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <string>
+#include <queue>
 #include <FastLED.h>
+#include <ESP32DMASPI/ESP32DMASPIMaster.h>
 
 constexpr uint32_t latch = 17;
 constexpr uint32_t latchPin = 1 << 17;
 constexpr uint32_t oe = 16;
 constexpr uint32_t reset = 4;
 
-void Task(void* params);
-void InitTouch();
-void GenerateBamData();
 void Update();
+void InitTouch();
 void UpdateTouch();
+void GenerateBamData();
+void IRAM_ATTR OnTimerInterrupt();
+void LedShowTask(void* args);
+void CheckBamData(uint8_t x, uint8_t y, uint8_t z, uint8_t rgb);
+inline void Latch();
 template <typename T>
 void PrintBinary(T v);
-void IRAM_ATTR OnTimerInterrupt();
 
-SPISettings setting(30 * pow(10, 6), MSBFIRST, SPI_MODE0);
+// ESP32DMASPI::Master master;
+// uint8_t* spi_master_tx_buf;
+// uint8_t* spi_master_rx_buf;
+SPISettings setting(20 * pow(10, 6), MSBFIRST, SPI_MODE0);
 hw_timer_t* timer = nullptr;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 volatile uint32_t interruptCounter = 0;
 uint32_t timerInterruptAt = 10;
 
-// [y][x][rgb]
-uint8_t data[8][8][3] = {0};
-// [y][rgb][digit]
-volatile uint8_t bamData[8][3][8] = {0};
+// [z][y][x]
+CRGB data[8][8][8] = {0};
+// [z][y][rgb][digit]
+volatile uint8_t bamData[8][8][3][8] = {0};
 volatile uint32_t outputChangeCount = 0;
 
 CRGB leds[32] = {0};
 uint8_t ledHue = 0;
 uint8_t ledBrightness = 63;
-TaskHandle_t hTask;
+uint8_t ledSpeed = 1;
+TaskHandle_t hLedShowTask;
 
-uint32_t touchPins[5] = {0, 2, 15, 13, 12};
-touch_pad_t touchPinTs[5] = {TOUCH_PAD_NUM1, TOUCH_PAD_NUM2, TOUCH_PAD_NUM3, TOUCH_PAD_NUM4, TOUCH_PAD_NUM5};
-bool touchData[5] = {0};
+constexpr uint32_t touchPinCount = 4;
+touch_pad_t touchPins[touchPinCount] = {TOUCH_PAD_NUM2, TOUCH_PAD_NUM3, TOUCH_PAD_NUM4, TOUCH_PAD_NUM5};
+uint16_t touchThresholds[touchPinCount];
+bool touchDatas[touchPinCount] = {0};
+
+volatile uint32_t aaa;
+volatile uint32_t bbb;
+
+typedef struct SV3
+{
+    int x;
+    int y;
+    int z;
+    SV3(int x, int y, int z) : x(x), y(y), z(z) {}
+} V3;
 
 void setup() {
     SPI.begin();
+    // spi_master_tx_buf = master.allocDMABuffer(64);
+    // spi_master_rx_buf = master.allocDMABuffer(64);
+
+    // master.setDataMode(SPI_MODE0);
+    // master.setFrequency(SPI_MASTER_FREQ_40M);
+    // master.setMaxTransferSize(64);
+    // master.setDMAChannel(1); // 1 or 2 only
+    // master.setQueueSize(1); // transaction queue size
+
+    // begin() after setting
+    // HSPI = CS: 15, CLK: 14, MOSI: 13, MISO: 12
+    // VSPI = CS: 5, CLK: 18, MOSI: 23, MISO: 19
+    // master.begin(VSPI);
+
     Serial.begin(38400);
+
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    pinMode(23, OUTPUT);
+    pinMode(18, OUTPUT);
     pinMode(oe, OUTPUT);
     pinMode(latch, OUTPUT);
     pinMode(reset, OUTPUT);
     digitalWrite(oe, LOW);
-    digitalWrite(latch, LOW);
+    digitalWrite(latch, HIGH);
     digitalWrite(reset, HIGH);
 
     pinMode(14, OUTPUT);
@@ -57,160 +109,9 @@ void setup() {
     timerAlarmWrite(timer, timerInterruptAt, true);
     timerAlarmEnable(timer);
 
-    // Remember there's task to utilize the other core
-    // xTaskCreatePinnedToCore(Task,   /* Task function. */
-    //                         "task", /* name of task. */
-    //                         10000,         /* Stack size of task */
-    //                         nullptr,       /* parameter of the task */
-    //                         10,            /* priority of the task */
-    //                         &hTask, /* Task handle to keep track of created task */
-    //                         0);            /* pin task to core 0 */      
+    xTaskCreatePinnedToCore(LedShowTask, "LedShowTask", 10000, nullptr, 10, &hLedShowTask, 0); 
 
     InitTouch();
-}
-
-void Task(void* params)
-{
-    while (1)
-    {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-}
-bool tt;
-void Update()
-{
-    static uint32_t lastUpdateTime = 0;
-    if (micros() - lastUpdateTime >= 100*1000)
-    {
-        lastUpdateTime = micros();
-        UpdateTouch();
-
-        static uint8_t hue = 0;
-        uint8_t t = 0;
-        // https://stackoverflow.com/questions/20420065/loop-diagonally-through-two-dimensional-array
-        for(uint32_t k = 0; k < 8 * 2; k++)
-        {
-            for(uint32_t j = 0; j <= k; j++)
-            {
-                uint32_t i = k - j;
-                if(i < 8 && j < 8)
-                {
-                    CRGB rgb = CHSV(hue + t * 4, 255, 255);
-                    data[i][j][0] = rgb.r;
-                    data[i][j][1] = rgb.g;
-                    data[i][j][2] = rgb.b;
-                }
-            }
-            t++;
-        }
-        hue += 4;
-
-        GenerateBamData();
-
-        for (uint32_t i = 0; i < 32; i++)
-            leds[i] = CHSV(ledHue + i * 8, 255, ledBrightness);
-        ledHue -= 2;
-        FastLED.show();
-    }
-}
-
-void InitTouch()
-{
-    touch_pad_init();
-    // GPIO 0 and 2 touch pins are always at 0, this line fixes the second pin
-    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V5);
-    for (uint32_t i = 0; i < 5; i++)
-        touch_pad_config(touchPinTs[i], 0);
-    touch_pad_filter_start(5);
-}
-
-void UpdateTouch()
-{
-    // touchAttachInterrupt()
-    for (uint32_t i = 1; i < 5; i++)
-    {
-        uint16_t value;// = touchRead(touchPins[i]);
-        touch_pad_read_filtered(touchPinTs[i], &value);
-        // Serial.print(value);
-        // Serial.print(' ');
-
-        if (value <= 300)
-        {
-            if (!touchData[i])
-            {
-                touchData[i] = true;
-                // ===== On pressed =====
-                // Serial.print(i + 1);
-                // Serial.println(" pressed");
-                switch (i)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                    {
-                        ledBrightness = min(ledBrightness + 25, 255); 
-                        break;
-                    }
-                    case 2:
-                    {
-                        ledBrightness = max(ledBrightness - 25, 0); 
-                        break;
-                    }
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                }
-            }
-        }
-        else
-        {
-            if (touchData[i])
-            {
-                touchData[i] = false;
-                // ===== On released =====
-                // Serial.print(i + 1);
-                // Serial.println(" released");
-                switch (i)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                }
-            }
-        }
-    }
-    // Serial.println();
-}
-
-void GenerateBamData()
-{
-    portENTER_CRITICAL(&timerMux);
-    for (uint32_t y = 0; y < 8; y++)
-        for (uint32_t x = 0; x < 8; x++)
-        {
-            uint8_t digit = 1 << x;
-            for (uint32_t rgb = 0; rgb < 3; rgb++)
-            {
-                uint32_t value = data[y][x][rgb];
-                for (uint32_t i = 0; i < 8; i++)
-                {
-                    if (value & 0x01)
-                        bamData[y][rgb][i] |= digit;
-                    else
-                        bamData[y][rgb][i] &= ~digit;
-                    value >>= 1;
-                }
-            }
-        }
-    portEXIT_CRITICAL(&timerMux);
 }
 
 void loop() {
@@ -228,88 +129,270 @@ void loop() {
                 timerAlarmWrite(timer, num, true);
                 break;
             }
-            case '6':
-                break;
         }
     }
-    // static uint32_t lastTouchTime = 0;
-    // if (micros() - 500000 >= lastTouchTime)
-    // {
-    //     lastTouchTime = micros();
-    //     Serial.println(touchRead(15));
-    // }
 
     Update();
-    
-    // static uint32_t tttt = 0;
-    // static uint32_t lastTime = 0;
-    // if (micros() - lastTime >= 1000000)
-    // {
-    //     lastTime = micros();
-
-    //     Serial.print("output changes: ");
-    //     Serial.println(outputChangeCount);
-    //     Serial.print("hz: ");
-    //     Serial.println(t / 8.0f);
-    //     outputChangeCount = 0;
-    //     Serial.print("interrupt count: ");
-    //     Serial.println(interruptCounter);
-    //     interruptCounter = 0;
-    //     Serial.print("ns taken to update: ");
-    //     Serial.println(tttt);
-    // }
 }
 
-// in normal loops:
-//
-// for (currentBamDigit = 0 to 7)
+void Update()
+{
+    static uint32_t lastUpdateTime = 0;
+    if (micros() - lastUpdateTime >= 50*1000)
+    {
+        lastUpdateTime = micros();
+        UpdateTouch();
+
+        static uint8_t hue = 0;
+        uint8_t t = 0;
+        // https://stackoverflow.com/questions/20420065/loop-diagonally-through-two-dimensional-array
+        // for(uint32_t indexesSum = 0; indexesSum < 8 * 2; indexesSum++)
+        // {
+        //     for(uint32_t oneIndex = 0; oneIndex <= indexesSum; oneIndex++)
+        //     {
+        //         uint32_t anotherIndex = indexesSum - oneIndex;
+        //         if(anotherIndex < 8 && oneIndex < 8)
+        //         {
+        //             data[anotherIndex][oneIndex] = CHSV(hue + t * 4, 255, 255);
+        //         }
+        //     }
+        //     t++;
+        // }
+
+        std::queue<V3> s;
+        int32_t visited[8][8][8] = {0};
+        s.emplace(0,0,0);
+        while (!s.empty())
+        {
+            V3 i = s.front();
+            s.pop();
+            int x = i.x, y = i.y, z = i.z;
+            if (!visited[x][y][z])
+            {
+                data[z][y][x] = CHSV(hue + t * 4, 255, 255);
+
+                visited[x][y][z] = true;
+                if (x+1 < 8)
+                {
+                    s.emplace(x+1, y, z);
+                    if (y+1 == 0 && z+1 == 0)
+                        t += 4;
+                }
+                if (y+1 < 8) s.emplace(x, y+1, z);
+                if (z+1 < 8) s.emplace(x, y, z+1);
+            }
+        }
+        hue += 4;
+
+        
+        for (uint32_t i = 0; i < 32; i++)
+            leds[i] = CHSV(ledHue - i * 8, 255, ledBrightness);
+        ledHue += ledSpeed;
+        
+        GenerateBamData();
+    }
+}
+
+void InitTouch()
+{
+    pinMode(0, INPUT);
+    touch_pad_init();
+    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V5);
+    for (uint32_t i = 0; i < touchPinCount; i++)
+    {
+        touch_pad_io_init(touchPins[i]);
+        touch_pad_config(touchPins[i], 0);
+    }
+    touch_pad_filter_start(5);
+    for (uint32_t i = 0; i < touchPinCount; i++)
+    {
+        uint16_t value;
+        touch_pad_read_filtered(touchPins[i], &value);
+        touchThresholds[i] = value / 3 * 2;
+    }
+}
+
+void UpdateTouch()
+{
+    static uint32_t lastTouchTime[touchPinCount] = {0};
+    uint16_t value;
+    for (uint32_t i = 0; i < touchPinCount; i++)
+    {
+        touch_pad_read_filtered(touchPins[i], &value);
+        if (value <= touchThresholds[i])
+        {
+            touchDatas[i] = true;
+            if (millis() - lastTouchTime[i] > 250)
+            {
+                Serial.printf("touchpad %u pressed, value: %u\n", i + 1, value);
+                lastTouchTime[i] = millis();
+                switch(i)
+                {
+                    case 0:
+                    {
+                        ledBrightness = min((int)ledBrightness + 25, 255); 
+                        break;
+                    }
+                    case 1:
+                    {
+                        ledBrightness = max((int)ledBrightness - 25, 1); 
+                        break;
+                    }
+                    case 2:
+                    {
+                        ledSpeed = min((int)ledSpeed + 1, 15); 
+                        break;
+                    }
+                    case 3:
+                    {
+                        ledSpeed = max((int)ledSpeed - 1, 0); 
+                        break;
+                    }
+                    // case 4:
+                    // {
+                    //     CheckBamData(0, 0, 0, 1);
+                    //     Serial.printf("micros taken for sending: %u\n", bbb - aaa);
+                    //     break;
+                    // }
+                }
+            }
+        }
+        else
+        {
+            if (touchDatas[i])
+            {
+                touchDatas[i] = false;
+                switch (i)
+                {
+                }
+            }
+        }
+    }
+}
+
+void GenerateBamData()
+{
+    portENTER_CRITICAL(&timerMux);
+    for (uint32_t z = 0; z < 8; z++)
+        for (uint32_t y = 0; y < 8; y++)
+            for (uint32_t x = 0; x < 8; x++)
+            {
+                uint8_t digit = 1 << x;
+                for (uint32_t rgb = 0; rgb < 3; rgb++)
+                {
+                    uint32_t value = data[z][y][x][rgb];
+                    for (uint32_t i = 0; i < 8; i++)
+                    {
+                        if (value & 0x01)
+                            bamData[z][y][rgb][i] |= digit;
+                        else
+                            bamData[z][y][rgb][i] &= ~digit;
+                        value >>= 1;
+                    }
+                }
+            }
+    portEXIT_CRITICAL(&timerMux);
+}
+
+// for (bamDigit = 0 to 7)
 //     for (layer = 1 to 8)
 //         bamCount = 0
-//         while (bamCount != ((1 << currentBamDigit) - 1))
+//         while (bamCount != ((1 << bamDigit) - 1))
 //             delay()
 //             bamCount++
-//         transfer(first layer => first shift reg. bit)
-//         transfer(first data bit => first shift reg. bit)
+//         transfer(face)
+//         transfer(layer)
 void IRAM_ATTR OnTimerInterrupt()
 {
     static uint32_t bamCount = 0;
-    static uint32_t currentBamDigit = 0;
-    static uint32_t currentY = 0;
+    static uint32_t bamDigit = 0;
+    static uint32_t y = 0;
+    // static uint8_t layer = 0;
 
     portENTER_CRITICAL_ISR(&timerMux);
     interruptCounter++;
 
     bamCount++;
-    if (bamCount == (1 << currentBamDigit))
+    if (bamCount == (1 << bamDigit))
     {
+        aaa = micros();
         SPI.beginTransaction(setting);
-        SPI.transfer(1 << currentY);
-        SPI.transfer(~bamData[currentY][0][currentBamDigit]);
-        SPI.transfer(~bamData[currentY][1][currentBamDigit]);
-        SPI.transfer(~bamData[currentY][2][currentBamDigit]);
-        GPIO.out_w1ts = latchPin;
-        GPIO.out_w1tc = latchPin;
+        uint32_t z = 8;
+        while (z)
+        {
+            z--;
+            SPI.transfer(bamData[z][y][2][bamDigit]);
+            SPI.transfer(bamData[z][y][1][bamDigit]);
+            SPI.transfer(bamData[z][y][0][bamDigit]);
+            // master.queue((uint8_t*)&bamData[z][y][2][bamDigit], 1);
+            // master.queue((uint8_t*)&bamData[z][y][1][bamDigit], 1);
+            // master.queue((uint8_t*)&bamData[z][y][0][bamDigit], 1);
+        }
+        SPI.transfer(1 << y);
+        // layer = 1 << y;
+        // master.queue((uint8_t*)&layer, 1);
+        // master.yield();
+        Latch();
         SPI.endTransaction();
         outputChangeCount++;
-        currentY++;
-        if (currentY >= 8)
+        y++;
+        if (y >= 8)
         {
-            currentY = 0;
-            currentBamDigit++;
-            if (currentBamDigit >= 8)
-                currentBamDigit = 0;
+            y = 0;
+            bamDigit++;
+            if (bamDigit >= 8)
+            {
+                bamDigit = 0;
+            }
         }
         bamCount = 0;
+        bbb = micros();
     }
     portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void LedShowTask(void* args)
+{
+    while (1)
+    {
+        FastLED.show();
+
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+}
+
+void CheckBamData(uint8_t x, uint8_t y, uint8_t z, uint8_t rgb)
+{
+    uint8_t targetData = data[z][y][x][rgb];
+    uint8_t dataToVerify = 0;
+    unsigned char dataToChar[9];
+    dataToChar[8] = '\0';
+    for(uint32_t bamDigit = 0; bamDigit < 8; bamDigit++)
+    {
+        uint8_t digit = (bamData[z][y][rgb][bamDigit] & (1 << x)) > 0;
+        dataToChar[bamDigit] = digit + '0';
+        dataToVerify |= digit << bamDigit;
+    }
+    Serial.printf("Verified a random BAM data at xyzrgb: %u,%u,%u,%u data: %3u bamdata: 0b%s result: %s\n",
+                  x, y, z, rgb, targetData, dataToChar,
+                  (targetData == dataToVerify) ? "SUCCEED  O" : "FAILED   X");
+}
+
+inline void Latch()
+{
+    GPIO.out_w1ts = latchPin;
+    GPIO.out_w1tc = latchPin;
 }
 
 template <typename T>
 void PrintBinary(T v)
 {
+    static char s[sizeof(T) * 8 + 1];
+    s[sizeof(T) * 8 - 1] = '\0';
     for (uint32_t i = 0; i < sizeof(T) * 8; i++)
     {
-        Serial.print((v & 1) ? '1' : '0');
+        s[i] = (v & 1) ? '1' : '0';
         v >>= 1;
     }
+    Serial.print(s);
 }
